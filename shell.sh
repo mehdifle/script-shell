ccefdf3239f725b786025a5d62c9fe5de6ca2d24
Mehdi Falegari <mehdi.falegari@gmail.com>
2024-02-17 13:05:12 +0330

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt update && apt upgrade -y
+apt install curl socat -y
+apt install cron
+curl https://get.acme.sh | sh
+~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+~/.acme.sh/acme.sh --register-account -m admin@loobi.biz
+~/.acme.sh/acme.sh --issue -d pwa.loobi.biz --standalone
+~/.acme.sh/acme.sh --installcert -d pwa.loobi.biz --key-file /root/private.key --fullchain-file /root/cert.crt
+bash bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
