59128d8025312b3f705cb5a75637fd988df9b39a
Mehdi Falegari <mehdi.falegari@gmail.com>
2024-02-18 13:15:24 +0330

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -8,4 +8,4 @@ curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --register-account -m admin@loobi.biz
 ~/.acme.sh/acme.sh --issue -d pwa.loobi.biz --standalone
 ~/.acme.sh/acme.sh --installcert -d pwa.loobi.biz --key-file /root/private.key --fullchain-file /root/cert.crt
-bash bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
