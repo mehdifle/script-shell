150dda532935c52aed4b0f8ca2eb8b0813ed49a5
Mehdi Falegari <m.falegari@ephoenix.ir>
2024-06-08 16:16:10 +0330

    fixed bug

--- outline.sh
+++ outline.sh
@@ -1,13 +1,17 @@
-while getopts k:p: flag
+while getopts k:p:n:t: flag
 do
     case "${flag}" in
         k) key=${OPTARG};;
         p) port=${OPTARG};;
+        n) newport=${OPTARG};;
+        t) newkey=${OPTARG};;
     esac
 done
 
 echo "$key"
 echo "$port"
+echo "$newport"
+echo "$newkey"
 
 apt install jq -y
 
@@ -22,6 +26,6 @@ do
         name=$(echo $Users_json | jq -r ".[$users_length_start].name")
         password=$(echo $Users_json | jq -r ".[$users_length_start].password")
         body='{"name":"'$name'","password":"'$password'","port":51618,"method":"chacha20-ietf-poly1305"}'
-        curl -X PUT "http://localhost:$port/$key/access-keys/$id" -H "Content-Type: application/json" -d $body --insecure
+        curl -X PUT "http://localhost:$newport/$newkey/access-keys/$id" -H "Content-Type: application/json" -d $body --insecure
         users_length_start=`expr $users_length_start + 1`
 done
