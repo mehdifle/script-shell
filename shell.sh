b9ca170096b0e26627c959490f82dd6e70a34117
Mehdi Falegari <mehdi.falegari@gmail.com>
2025-06-14 12:41:59 +0330

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -7,9 +7,9 @@ apt install nginx -y
 systemctl enable nginx
 curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-~/.acme.sh/acme.sh --register-account -m admin@loobi.biz
+~/.acme.sh/acme.sh --register-account -m admin@loobi.space
 service nginx stop
-~/.acme.sh/acme.sh --issue -d pwa.loobi.biz --standalone
-~/.acme.sh/acme.sh --installcert -d pwa.loobi.biz --key-file /root/private.key --fullchain-file /root/cert.crt
+~/.acme.sh/acme.sh --issue -d socket.loobi.space --standalone
+~/.acme.sh/acme.sh --installcert -d socket.loobi.space --key-file /root/private.key --fullchain-file /root/cert.crt
 service nginx start
 bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
